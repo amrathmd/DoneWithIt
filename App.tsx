c11a80b136ec0d15ab96d3d5d3cb38d06d5e6c65
amrathmd <amrathmd.mohammed@gmail.com>
2024-01-08 06:26:40 +0530

    Initial Commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image, Platform, Dimensions, Touchable, TouchableWithoutFeedback, Alert, TouchableOpacity, ImageBackground } from 'react-native';
 
 export default function App() {
+  const handleYes = ()=>{
+    console.log("yes");
+  }
+  const handleNo = () =>{
+    console.log("no")
+  }
+  const handleTouchable = ()=>{
+    Alert.alert('The title','The message',[{text: 'yes' ,onPress: handleYes},{text:'No' , onPress: handleNo}]);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    
+        <ImageBackground
+          source={require('./assets/landingPage1.jpg')}
+          style={styles.landingImage}
+        >
+        <TouchableOpacity style={styles.login}>
+          <View style={{flex: 1,alignItems:'center',justifyContent: 'center'}}>
+            <Text style={{fontSize:20,fontFamily:'monospace',color:'white'}}>Login</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity  style={styles.signin}>
+          <View style={{flex: 1,alignItems:'center',justifyContent: 'center'}}>
+            <Text style={{fontSize:20,fontFamily:'monospace',color:'white'}}>SignUp</Text>
+          </View>
+        </TouchableOpacity>
+        </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  landingImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    height: Dimensions.get('screen').height,
+    width: Dimensions.get('screen').width,
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
+  login: {
+    position: 'absolute',
+    bottom: 50,
+    backgroundColor: 'rgba(255, 0, 0, 0.8)',
+    padding: 10,
+    borderRadius: 5,
+    width: 300,
+    height: 70
+  },
+  signin:{
+    position: 'absolute',
+    bottom : 150,
+    backgroundColor: 'rgba(0, 255, 0, 0.8)',
+    padding: 10,
+    borderRadius: 5,
+    width: 300,
+    height: 70
+  }
 });
